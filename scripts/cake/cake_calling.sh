668ead4bf8e213de14a5eeff032cc1da3832c967
mlafave <matt.lafave@gmail.com>
2015-01-08 17:28:25 -0500

    Fixed directory naming to not conflict with input, and removed mutect reference

--- scripts/cake/cake_calling.sh
+++ scripts/cake/cake_calling.sh
@@ -11,14 +11,15 @@ SAMPLEFILE=$1
 OUT_DIR=`basename ${SAMPLEFILE}`
 OUT_DIR=`echo $OUT_DIR | sed 's/\..*//'`
 
-mkdir $OUT_DIR
+
+mkdir ${OUT_DIR}_dir
 
 perl /opt/cake/Cake/trunk/scripts/run_somatic_pipeline.pl \
 	-s $SAMPLEFILE \
 	-species human \
-	-callers mpileup,varscan,bambino,somaticsniper,mutect \
+	-callers mpileup,varscan,bambino,somaticsniper \
 	-separator "," \
-	-o $OUT_DIR \
+	-o ${OUT_DIR}_dir \
 	-mode CALLING
 
 
